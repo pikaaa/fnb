f16bf22ec35ed1392039857a6b04ed5d4466961c
Priyanka Sabhagani <priyanka.sabhagani@bookmyshow.com>
2017-11-27 18:38:21 +0530

    cleaning

--- components/animatedDetailedView.js
+++ components/animatedDetailedView.js
@@ -12,20 +12,10 @@ import FnBTableCell from './fnbtablecell';
 class AnimatedDetailedView extends Component{
   constructor(props) {
     super(props);
-    // const position = new Animated.ValueXY();
-    // const panResponder = PanResponder.create({
-    //   onStartShouldSetPanResponder: () => true,
-    //   onPanResponderMove: (event, gesture) => {
-    //     console.log(gesture);
-    //     position.setValue({x: gesture.dx, y: 16});
-    //   },
-    //   onPanResponderRelease: () => {}
-    // })
+
     this.state = {
       data: ['ALL','COMBO', 'SNACKS', 'BEVERAGES'],
       selectedIndex: 0,
-      // panResponder: panResponder,
-      // position: position
     };
   }
 captureRef = (ref) => { this.scrollViewRef = ref;};
@@ -45,7 +35,7 @@ captureRef = (ref) => { this.scrollViewRef = ref;};
         >
         {this.state.data.map(
          () =>
-            <View style = {{flexDirection: 'column', width: Dimensions.get('window').width}} id = {0}>
+            <View style = {{flexDirection: 'column', width: Dimensions.get('window').width}} >
                <Text style={{color: 'black'}}>{this.state.data[this.props.selectedIndex]} </Text>
                <FnBTableCell/>
              </View>
@@ -61,12 +51,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'stretch'
   },
-detailedView: {
-  // flex: 20,
-  // backgroundColor: 'blue',
-  // marginBottom: 20,
-  // marginTop: 10,
-}
 });
 
 export default AnimatedDetailedView;
